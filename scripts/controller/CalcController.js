5642b3275a9552dbe93e1ef304771c08de02391f
Douglas <douglasvitor.dev@gmail.com>
2022-06-20 11:16:34 -0300

    Section 2.6: Set the display date and time

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,20 +1,58 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = "0";
+        this._locale = 'pt-BR'
+
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
+
+        this.initialize()
+    }
+
+    initialize() {
+        this.setDisplayTimeAndDate();
+        setInterval(() => {
+            this.setDisplayTimeAndDate();
+        }, 1000);
+    }
+    
+    setDisplayTimeAndDate() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value) {
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayTime() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayTime(value) {
+        this._dateEl.innerHTML = value;
     }
 
     get currentDate() {
-        return this._displayCalc;
+        return new Date();
     }
 
     set currentDate(value) {
